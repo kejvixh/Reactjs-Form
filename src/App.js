cc641d22a1906cec94128faa5ccf8face0a9c702
kejvixh <kejvixhelilaj@outlook.com>
2021-10-29 12:54:37 +0200

    full comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import map from 'lodash/map';
+import startCase from 'lodash/startCase';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FormControl} from 'react-bootstrap';
+import CreatableSelect from 'react-select/creatable';
+
+const Input = ({label, id, handleChange, name, type}) => (
+  <>
+    <label htmlFor={id}>{label}</label>
+    <FormControl type={type || 'text'} id={id} name={name || id} onChange={handleChange} />
+    <br />
+    
+  </>
+);
 
 function App() {
+  const [form, setFormValue] = useState({name: '', lastname: '', email: '', });
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setFormValue((prevFormValues) => ({
+      ...prevFormValues,
+      [name]: value,
+    }));
+  };
+  const hoobies=[
+    { value: 'Movies', label : 'Movies' },
+    { value: 'Fitness', label : 'Fitness' },
+    { value: 'Picnics', label : 'Picnics' },
+    { value: 'Singing', label : 'Singing' }
+ ];
+ const [selectedOption, setSelectedOption] = useState(null);
+ const handleChange1 = e => {
+  setSelectedOption(e);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      {map(form, (val, key) => (
+        <p key={key}>
+          {`${startCase(key)}: `}
+          {val}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      ))}
+      
+      <Input label="Name: " id="name" handleChange={handleChange} />
+      <Input label="Last Name: " id="lastname" handleChange={handleChange} />
+      <Input label="Email: " id="email" handleChange={handleChange} />
+      <CreatableSelect
+      isMulti
+      id="options"
+      options={hoobies}
+      value={selectedOption}
+      onChange={handleChange1} />
+      <b>Selected Options</b><br />
+        <pre>{JSON.stringify(selectedOption, null, 2)}</pre>
+    </>
   );
 }
 
